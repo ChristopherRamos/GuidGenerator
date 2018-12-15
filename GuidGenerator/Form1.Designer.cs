c75b52de6a6c83bd030a743a0f3f29e6513da2bf
Chris Ramos <cramos@starbucks.com>
2018-12-14 22:27:56 -0800

    Initial Commit

---
+++ GuidGenerator/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace GuidGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuid = new System.Windows.Forms.Button();
+            this.txtGuid = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGuid
+            // 
+            this.btnGuid.Location = new System.Drawing.Point(276, 95);
+            this.btnGuid.Name = "btnGuid";
+            this.btnGuid.Size = new System.Drawing.Size(215, 38);
+            this.btnGuid.TabIndex = 0;
+            this.btnGuid.Text = "Generate GUID";
+            this.btnGuid.UseVisualStyleBackColor = true;
+            this.btnGuid.Click += new System.EventHandler(this.btnGuid_Click);
+            // 
+            // txtGuid
+            // 
+            this.txtGuid.Location = new System.Drawing.Point(62, 36);
+            this.txtGuid.Name = "txtGuid";
+            this.txtGuid.Size = new System.Drawing.Size(605, 26);
+            this.txtGuid.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 170);
+            this.Controls.Add(this.txtGuid);
+            this.Controls.Add(this.btnGuid);
+            this.Name = "Form1";
+            this.Text = "Chris\' GUID Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuid;
+        private System.Windows.Forms.TextBox txtGuid;
+    }
+}
+
---
+++ GuidGenerator/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuidGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuid_Click(object sender, EventArgs e)
+        {
+            string id = Guid.NewGuid().ToString();
+            txtGuid.Clear();
+            txtGuid.Text = id;
+
+        }
+    }
+}
